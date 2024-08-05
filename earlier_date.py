63942faf3a6a45d98c73570b9fdaea88515c1039
akshaya2248 <146075091+akshaya2248@users.noreply.github.com>
2024-08-05 12:21:17 +0530

    Delete earlier_date.py

--- earlier_date.py
+++
@@ -1,9 +0,0 @@
-# Write a program to determine which one is the earlier date from the two given dates.
-
-date1=input("enter the first date(YYYY-MM-DD):")
-date2=input("enter the second date(YYsYY-MM-DD):")
-if(date1 < date2):
-    print("the earlier date is",date1)
-else:
-    print("the earlier date is",date2)
-    
